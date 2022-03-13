01d88e9439876c2ee2db7fadc9f6dbd730864809
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2022-03-14 12:31:23 +1300

    Psudo Code written Questions 1-10 added. Variables added. Added 3 levels.

--- Te Reo Maori Quiz Project/Program.cs
+++ Te Reo Maori Quiz Project/Program.cs
@@ -1,2 +1,139 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* This code will do
+Add up correct scores and display final score.
+Have 10 question in relation to Te Reo Maori with multiple options in a question.
+Question 1-3 is basic level. Questions 4-7 is medium level. Questions 8-10 insane level.
+Will full/fool proof code as much as possiable.
+Push codes to Github.
+Have multiple methods and have less variable to be cpu efficient. 
+*/
+
+//declare variables
+string answers;
+
+// Introduction
+Console.WriteLine("Hello and welcome to my Te Reo Maori Quiz. ");
+Console.ReadLine();
+Console.WriteLine("There will be 10 questions that you will be answering. ");
+Console.ReadLine();
+Console.WriteLine("Question 1-3 basic level. ");
+Console.WriteLine("Quetions 4-7 medium level. ");
+Console.WriteLine("Question 8-10 insane level. ");
+Console.ReadLine();
+
+// Basic Level Questions
+Console.WriteLine("You know are answering Basic Level Questions. ");
+Console.WriteLine("Question 1. ");
+Console.WriteLine("What is ocean in Te Reo Maori: ");
+Console.WriteLine("Ocean");
+Console.WriteLine("Moana");
+Console.WriteLine("Awa");
+Console.ReadLine();
+
+Console.WriteLine("Question 2. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+Console.WriteLine("Question 3. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+// Basic Level Complete
+Console.WriteLine("Basic Level Completed, Congratulations. ");
+Console.ReadLine();
+
+// Medium Level Questions
+Console.WriteLine("You now are answering Medium Level Question. ");
+Console.WriteLine("Question 4. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+Console.WriteLine("Question 5. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+Console.WriteLine("Question 6. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+Console.WriteLine("Question 7. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+// Medium Level Completed
+Console.WriteLine("Medium Level Completed, Congratulations. ");
+Console.ReadLine();
+
+// Insane Level Questions
+Console.WriteLine("Question 8. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+Console.WriteLine("Question 9. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+Console.WriteLine("Question 10. ");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.ReadLine();
+
+// Finished Quiz
+Console.WriteLine("Congratulations you have completed my Te Reo Maori Quiz I hope you have learnt something about Te Re Maori. ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
