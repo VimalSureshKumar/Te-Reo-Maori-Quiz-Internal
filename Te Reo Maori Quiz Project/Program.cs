aa017238a01c7069b8f1e393661a5df22fb8eb11
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2022-03-17 15:14:48 +1300

    Fixed Master Branch Problem. Code Working As Usual.

--- Te Reo Maori Quiz Project/Program.cs
+++ Te Reo Maori Quiz Project/Program.cs
@@ -1,28 +1,31 @@
 ﻿
-/* This progame is going to be a Te Reo Maori Quiz.
-This programe adds up correct scores and displays final score.
-Have 20 question in relation to Te Reo Maori with multiple options in a question.
-Question 1-5 is basic level. Questions 6-12 is medium level. Questions 13-20 Extreme level.
-Will full/fool proof code as much as possiable.
-Push codes to Github.
-Have multiple methods and have less variable to be cpu efficient. 
-*/
-
 {
     {
+
         {
             // declare variable
 
 
             // Introduction
-            Console.WriteLine("Hello and welcome to my Te Reo Maori Quiz. ");
+            Console.WriteLine("||========================================================|| ");
+            Console.WriteLine("||Kia Ora And Welcome To My Te Reo Maori Quiz.            || ");
+            Console.WriteLine("||                                                        || ");
+            Console.WriteLine("||There Will Be 20 Questions That You Will Be Answering.  || ");
+            Console.WriteLine("||Question 1-5 Basic level.                               || ");
+            Console.WriteLine("||Quetions 6-12 Medium level.                             || ");
+            Console.WriteLine("||Question 15-20 Extreme level.                           || ");
+            Console.WriteLine("||                                                        || ");
+            Console.WriteLine("||Good Luck                       PRESS ENTER TO CONTINUE || ");
+            Console.WriteLine("||========================================================|| ");
             Console.ReadLine();
             //if or switch statements.
             //loops and arrays.
             //methods for adding up correct scores.
 
             // Basic Level Questions
-            Console.WriteLine("You know are answering Basic Level Questions. ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("You know are answering Basic Level Questions, Good Luck. ");
+            Console.WriteLine("PRESS ENTER TO CONTINUE: ");
             Console.ReadLine();
             Console.WriteLine("Question 1: ");
             Console.WriteLine("What is ocean in Te Reo Maori: ");
@@ -30,194 +33,205 @@ Have multiple methods and have less variable to be cpu efficient.
             Console.WriteLine("b) Moana");
             Console.WriteLine("c) Awa");
             Console.WriteLine("d) River");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 2: ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)     ");
+            Console.WriteLine("b)     ");
+            Console.WriteLine("c)     ");
+            Console.WriteLine("d)     ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 3. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)     ");
+            Console.WriteLine("b)     ");
+            Console.WriteLine("c)     ");
+            Console.WriteLine("d)     ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 4. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)      ");
+            Console.WriteLine("b)      ");
+            Console.WriteLine("c)      ");
+            Console.WriteLine("d)      ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 5. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)       ");
+            Console.WriteLine("b)       ");
+            Console.WriteLine("c)       ");
+            Console.WriteLine("d)       ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
+            Console.ReadLine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("PRESS ENTER FOR NEXT LEVEL: ");
             Console.ReadLine();
 
             // Medium Level Questions
-            Console.WriteLine("You now are answering Medium Level Question. ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("You now are answering Medium Level Questions, Good Luck. ");
+            Console.WriteLine("PRESS ENTER TO CONTINUE: ");
             Console.ReadLine();
             Console.WriteLine("Question 6. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)       ");
+            Console.WriteLine("b)       ");
+            Console.WriteLine("c)       ");
+            Console.WriteLine("d)       ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 7. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)        ");
+            Console.WriteLine("b)        ");
+            Console.WriteLine("c)        ");
+            Console.WriteLine("d)        ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 8. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
             Console.ReadLine();
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)        ");
+            Console.WriteLine("b)        ");
+            Console.WriteLine("c)        ");
+            Console.WriteLine("d)        ");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 9. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)         ");
+            Console.WriteLine("b)         ");
+            Console.WriteLine("c)         ");
+            Console.WriteLine("d");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 10. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 11. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 12. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
+            Console.ReadLine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("PRESS ENTER FOR NEXT LEVEL: ");
             Console.ReadLine();
 
-            // Insane Level Questions
-            Console.WriteLine("You now are answering Insane Level Questions, Good Luck. ");
+            // Extreme Level Questions
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("You now are answering Extreme Level Questions, Good Luck. ");
+            Console.WriteLine("PRESS ENTER TO CONTINUE: ");
             Console.ReadLine();
             Console.WriteLine("Question 13. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 14. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 15. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 16. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 17. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 18. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 19. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PRESS ENTER TO CONFIRM: ");
             Console.ReadLine();
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 20. ");
             Console.WriteLine("What is --- in Te Reo Maori: ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("Type either A, B, C, D in keybord, Press Enter To Continue: ");
+            Console.WriteLine("a)");
+            Console.WriteLine("b)");
+            Console.WriteLine("c)");
+            Console.WriteLine("d)");
+            Console.WriteLine("Type either A, B, C, D in keybord, PPRESS ENTER TO CONFIRM: ");
+            Console.ReadLine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("PRESS ENTER TO SEE RESULTS: ");
             Console.ReadLine();
 
-            // Final Tally Of Score
+            // Final Tally Of Score 
+            Console.WriteLine("Your Total Amount of Points earned: ");
             Console.WriteLine("Your Total Amount of Score earned was: ");
-            Console.ReadLine();
 
             // Finished Quiz
             Console.WriteLine("Congratulations you have completed my Te Reo Maori Quiz I hope you have learnt something about Te Re Maori. ");
-            Console.ReadKey();
         }
     }
 }
