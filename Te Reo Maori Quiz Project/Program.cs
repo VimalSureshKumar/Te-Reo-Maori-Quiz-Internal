b52895a17d080cfa9b8fe0c2fcbb744a62c614e4
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2022-03-28 12:30:29 +1300

    If Statement For Correct Answers Added For Questions 1-20.

--- Te Reo Maori Quiz Project/Program.cs
+++ Te Reo Maori Quiz Project/Program.cs
@@ -25,17 +25,11 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
-            int answer1 = 2;
-            if (answer1 == 2)
+            int question1 = 2;
+            if (question1 == 2)
             {
                 Console.WriteLine("Correct Answer");
-                score = score + 1;
-            }
-            else
-            {
-                int incorrectAnswer1 = (1 + 2 + 4);
-                Console.WriteLine("Incorrect Answer");
-                score = score + 0;
+                score = score++;
             }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 2:\nWhat is Mother in Te Reo Maori:\n1) Whaea\n2) Whanau\n3) Tamaraki\n4) Mama\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 1) and 4) is correct answer
@@ -43,11 +37,11 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
-            int question4 = 1 + 4;
-            int answer2 = 4;
-            if (answer1 >= 1 || answer2 == 4)
+            int question2 = 1 + 4;
+            if (question1 >= 1 || question2 == 4)
             {
                 Console.WriteLine("Correct Answer");
+                score = score++;
             }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 3:\nWhat is Children in Te Reo Maori:\n1) Tamaraki \n2) Children \n3) Tena Koe \n4) Ka Kite \nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 1) is correct answer
@@ -55,17 +49,35 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question3 = 1;
+            if (question3 == 1)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 4:\nWhat is Good Morning in Te Reo Maori:\n1) Ahiahi Pai \n2) Kia Ora \n3) Morena \n4) Ra Pai \nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 3) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question4 = 3;
+            if (question4 == 3)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 5:\nWhat is School in Te Reo Maori:\n1) Whare\n2) Kura\n3) Hapu\n4) Tane\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 2) is correct answer  
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question5 = 2;
+            if (question5 == 2)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             // Medium Level Questions
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -79,11 +91,23 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question6 = 2;
+            if (question6 == 2)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 7:\nWhat is Prayer in Te Reo Maori:\n1) Karakia\n2) Rongoa\n3) Raupo\n4) Raupatu\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 1) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question7 = 1;
+            if (question7 == 1)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 8:\nWhat is Sovereighty in Te Reo Maori:\n1) Rangatahi\n2) Rangatira\n3) Rahui\n4) Rangatiratanga\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 4) is correct answer
@@ -91,11 +115,23 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question8 = 4;
+            if (question8 == 4)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 9:\nWhat is Crying in Te Reo Maori:\n1) Porohe\n2) Tangihanga\n3) Piharau\n4) Patiki\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 2) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question9 = 2;
+            if (question9 == 2)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 10:\nWhat is Ancestry in Te Reo Maori:\n1) Panui\n2) Pa Tuna\n3) Whakapapa\n4) Ngawha\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 3) is correct answer
@@ -103,17 +139,35 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question10 = 3;
+            if (question10 == 3)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 11:\nWhat is Embarrassed in Te Reo Maori:\n1) Whakama\n2) Wairua\n3) Korowai\n4) Korimako\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 1) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question11 = 1;
+            if (question11 == 1)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 12:\nWhat is Treasure in Te Reo Maori:\n1) Korero\n2) Kokopu\n3) Tangi\n4) Taonga\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 4) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question12 = 4;
+            if (question12 == 4)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             // Extreme Level Questions
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -126,11 +180,23 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question13 = 3;
+            if (question13 == 3)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 14:\nWhat is Shiver in Te Reo Maori:\n1) Tungawiri\n2) Whakaoreone\n3) Totohu\n4) Kauhuri\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 1) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question14 = 1;
+            if (question14 == 1)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 15:\nWhat is Slide in Te Reo Maori:\n1) Hinga\n2) Ngoki\n3) Ruru\n4) Koneke\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 4) is correct answer
@@ -138,11 +204,23 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question15 = 4;
+            if (question15 == 4)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 16:\nWhat is Thrust in Te Reo Maori:\n1) Hinga\n2) Takahi\n3) Kokiri\n4) Matike\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 3) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question16 = 3;
+            if (question16 == 3)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 17:\nWhat is Bounce in Te Reo Maori:\n1) Matike\n2) Tupana\n3) Mawewe\n4) Takapori\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 2) is correct answer
@@ -150,11 +228,23 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question17 = 2;
+            if (question17 == 2)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 18:\nWhat is Freeze in Te Reo Maori:\n1) Wheta\n2) Iriri\n3) E Peke\n4) Kia Mau\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 4) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question18 = 4;
+            if (question18 == 4)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 19:\nWhat is in Te Reo Maori:\n1) Wakaoreore\n2) Ruru\n3) Huri\n4) Pehi\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 1) is correct answer
@@ -162,11 +252,23 @@
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
             }
+            int question19 = 1;
+            if (question19 == 1)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
+            }
             Console.WriteLine("=========================================================");
             Console.WriteLine("Question 20:\nWhat is Shimmy in Te Reo Maori:\n1) Hurokurku\n2) E Heke Ki Raro\n3) Titakataka\n4) Whakarewa\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 3) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5)
             {
                 Console.Write("\nPlease Enter Either 1, 2, 3, 4 In The Keyboard: ");
+            }
+            int question20 = 3;
+            if (question20 == 3)
+            {
+                Console.WriteLine("Correct Answer");
+                score = score++;
             }
             // Final Tally Of Score 
             Console.WriteLine("=================================================");
