7b81cf720e7583acd78b6f9d0c3c9acce8013962
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2022-05-23 12:40:53 +1200

    Added Switch Statement For Level. Made Array For Each Level and Other Chnages Added

--- Te Reo Maori Quiz Project/Program.cs
+++ Te Reo Maori Quiz Project/Program.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace TeReoMaoriQuizProject
 {
-    Menu();
-    MediumLevel();
-    ExtremeLevel();
     class MaoriQuiz
     {
         static void Main(string[] args)
         {
+            Menu();
+            MediumLevel();
+            ExtremeLevel();
 
             // Psudo Code
             /* This program is a 30 Question Quiz On Te Reo Maori. 
@@ -48,8 +48,6 @@ namespace TeReoMaoriQuizProject
             // Add Array For Each Level To Replace Variables Into Shorter Length
 
             // Declare Variables
-            char levelOption;
-            char userOption;
             int option;
             int score = 0;
             int question1 = 2;
@@ -74,20 +72,81 @@ namespace TeReoMaoriQuizProject
             int question19 = 1;
             int question20 = 3;
 
-            static void Menu() { }
-            // Introduction
-            Console.WriteLine("========================================================");
-            Console.WriteLine("Kia Ora And Welcome To My Te Reo Maori Quiz.\nThere Will Be 20 Questions That You Will Be Answering." +
-            "\nQuestion 1-5 Basic level.\nQuetions 6-12 Medium level.\nQuestion 13-20 Extreme level.\n");
-            Console.WriteLine("SELECT WHICH LEVEL YOU WANT TO DO:\nPress 1 For BASIC LEVEL.\nPress 2 FOR MEDIUM LEVEL.\nPress 3 FOR EXTREME LEVEL.");
-            Console.WriteLine("========================================================");
+            static void Menu() 
+            {
+                // Introduction
+                Console.WriteLine("========================================================");
+                Console.WriteLine("Kia Ora And Welcome To My Te Reo Maori Quiz.\nThere Will Be 30 Questions In This Quiz.\n10 Questions Per Level." +
+                "\n========================================================\n");
+                Console.WriteLine("SELECT A LEVEL:\n\nENTER 1 For BASIC LEVEL.\n\nENTER 2 FOR MEDIUM LEVEL.\n\nENTER 3 FOR EXTREME LEVEL.\n");
+                Console.WriteLine("========================================================");
+                string chooseLevel;
+                chooseLevel = Console.ReadLine();
+                switch (chooseLevel)
+                {
+                    case "1":
+                        Console.WriteLine("You Have Selected Basic Level!\nPress Enter To Continue:");
+                        Console.Clear();
+                        BasicLevel();
+                        break;
+                    case "2":
+                        Console.WriteLine("You Have Selected Medium Level!\nPress Enter To Continue:");
+                        Console.Clear();
+                        MediumLevel();
+                        break;
+                    case "3":
+                        Console.WriteLine("You Have Selected Extreme Level!\nPress Enter To Continue:");
+                        Console.Clear();
+                        ExtremeLevel();
+                        break;
+                    default:
+                        Console.WriteLine("PLEASE ENTER A VALID OPTION: ");
+                        break;
+                }
 
-            // Basic Level Questions
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine("You Know Are Answering Basic Level Questions, Good Luck.\nPRESS ENTER TO CONTINUE: ");
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.ReadLine();
-            Console.WriteLine("=========================================================");
+            }
+            static void BasicLevel()
+            {
+                string[] questions = { "Question 1:\nWhat is Ocean in Te Reo Maori:, Question 2:\nWhat is Mother in Te Reo Maori:, Question 3:\nWhat is Children in Te Reo Maori:, Question 4:\nWhat is Good Morning in Te Reo Maori:, Question 5:\nWhat is School in Te Reo Maori: " };
+                string[] options = { "\n1) Ocean\n2) Moana\n3) Awa\n4) River, \n1) Whaea\n2) Whanau\n3) Tamaraki\n4) Mama, \n1) Tamaraki \n2) Children \n3) Tena Koe \n4) Ka Kite", "n1) Ahiahi Pai \n2) Kia Ora \n3) Morena \n4) Ra Pai","\n1) Whare\n2) Kura\n3) Hapu\n4) Tane"};
+                string[] answers = { "2, 1, 4, 1, 3, 2" };
+
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("You Know Are Answering Basic Level Questions, Good Luck.\nPRESS ENTER TO CONTINUE: ");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.ReadLine();
+                Console.WriteLine("=========================================================");
+
+            }
+
+            static void MediumLevel()
+            {
+                string[] questions = { "Question 1:\nWhat is Guardian in Te Reo Maori:, Question 2:\nWhat is Prayer in Te Reo Maori:, Question 3:\nWhat is Sovereighty in Te Reo Maori:", "Question 4:\nWhat is Crying in Te Reo Maori:, Question 5:\nWhat is Ancestry in Te Reo Maori:", "Question 6:\nWhat is Embarrassed in Te Reo Maori:, Question 7:\nWhat is Treasure in Te Reo Maori:" };
+                string[] options = { "\n1) Tane\n2) Kaitiaki\n3) Taniwha\n4) Ruru, \n1) Karakia\n2) Rongoa\n3) Raupo\n4) Raupatu, n1) Rangatahi\n2) Rangatira\n3) Rahui\n4) Rangatiratanga", "\n1) Porohe\n2) Tangihanga\n3) Piharau\n4) Patiki", "\n1) Panui\n2) Pa Tuna\n3) Whakapapa\n4) Ngawha", "\n1) Whakama\n2) Wairua\n3) Korowai\n4) Korimako, \n1) Korero\n2) Kokopu\n3) Tangi\n4) Taonga" };
+                string[] answers = { "2, 1, 4, 2, 3, 1, 4 " };
+
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("You Now Are Answering Medium Level Questions, Good Luck.\nPRESS ENTER TO CONTINUE: ");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.ReadLine();
+                Console.WriteLine("=========================================================");
+
+            }
+
+            static void ExtremeLevel()
+            {
+                string[] questions = { "" };
+                string[] options = { "" };
+                string[] answers = { "" };
+
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("You Now Are Answering Extreme Level Questions, Good Luck.\nPRESS ENTER TO CONTINUE: ");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.ReadLine();
+                Console.WriteLine("=========================================================");
+
+            }
+
             Console.WriteLine("Question 1:\nWhat is Ocean in Te Reo Maori:\n1) Ocean\n2) Moana\n3) Awa\n4) River" +
             "\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 2) Moana is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5) // While Loop So The User Is Only Allowed to Enter (1, 2, 3, 4)
@@ -169,12 +228,6 @@ namespace TeReoMaoriQuizProject
                 Console.WriteLine("Incorrect Answer!");
             }
 
-            // Medium Level Questions & Message
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine("You Now Are Answering Medium Level Questions, Good Luck.\nPRESS ENTER TO CONTINUE: ");
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.ReadLine();
-            Console.WriteLine("=========================================================");
             Console.WriteLine("Question 6:\nWhat is Guardian in Te Reo Maori:\n1) Tane\n2) Kaitiaki\n3) Taniwha\n4) Ruru" +
             "\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 2) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5) // While Loop So The User Is Only Allowed to Enter (1, 2, 3, 4)
@@ -288,11 +341,6 @@ namespace TeReoMaoriQuizProject
             }
 
             // Extreme Level Questions & Message & Message
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine("You Now Are Answering Extreme Level Questions, Good Luck.\nPRESS ENTER TO CONTINUE: ");
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.ReadLine();
-            Console.WriteLine("=========================================================");
             Console.WriteLine("Question 13:\nWhat is Circle in Te Reo Maori:\n1) Pahu\n2) Ata Toro\n3) Porohita\n4) Meke" +
             "\nType either 1, 2, 3, 4 in Keybord, PRESS ENTER TO CONFIRM: "); // 3) is correct answer
             while (!int.TryParse(Console.ReadLine(), out option) || option <= 0 || option >= 5) // While Loop So The User Is Only Allowed to Enter (1, 2, 3, 4)
